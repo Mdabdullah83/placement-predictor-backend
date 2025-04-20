cd26cb94708c8ce97808f1a66988daeb20788d58
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2025-04-21 01:53:55 +0530

    adding the routes

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -4,8 +4,11 @@ const { login, ViewAdminDetails } = require("../controller/adminController");
 const { registerUser, loginUser } = require("../controller/userController");
 const quizRoutes=require("./quizRoutes");
 
-
+//user routes
 Router.post("/register", registerUser);
 Router.post("/login", loginUser);
+
+//quiz routes
 Router.use("/quiz",quizRoutes)
+
 module.exports = Router;
